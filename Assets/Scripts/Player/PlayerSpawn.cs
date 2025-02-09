c6055ec2eee9f1ea0a207fbaf5fe33c332d2ceeb
kitason0722 <riku.kitamura.3g@stu.hosei.ac.jp>
2025-02-09 18:31:40 +0900

    スポーンエリア内のランダムな座標にプレイヤーがスポーンするように修正
    スポーンエリアの半径も修正

--- Assets/Scripts/Player/PlayerSpawn.cs
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject playerprefab;
-    [SerializeField] private BulletPool bulletPool;
+    [SerializeField] private GameObject playerprefab;//プレイヤーのプレハブ
+    [SerializeField] private BulletPool bulletPool;//弾のプール
+    [SerializeField] private float radius = 3.0f;//プレイヤーの生成半径
     private int playersize = 4;//プレイヤーの数
     private void Awake()
     {
         for(int i = 0;i < playersize;i++)
         {
-            GameObject instance = Instantiate(playerprefab,transform.position,transform.rotation);
+            //スポーンエリア内の範囲内のランダムな位置を取得
+            Vector2 randompos = Random.insideUnitCircle * radius;
+            Vector3 spawnpos = new Vector3(randompos.x, randompos.y, 0) + transform.position;
+
+            //プレイヤーの生成
+            GameObject instance = Instantiate(playerprefab,spawnpos,transform.rotation);
             instance.name = $"{playerprefab.name}_{i}";
 
+            //プレイヤーの変数の設定
             PlayerControl playerControl = instance.GetComponent<PlayerControl>();
             if(i==0)playerControl.isPlayer = true;
             else playerControl.isPlayer = false;
